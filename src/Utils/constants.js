eaecfc81aa1439d6ca141b5bea348d9d309ce62c
Lucky Pareek <kunalpareek.21@gmail.com>
2023-11-30 14:50:11 +0530

    Added env file

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,10 +1,10 @@
- export const  API_KEY="AIzaSyBnz2RSRoyRpN8ofvYtfutbIn-S-lBAINI"
+ export const  API_KEY=process.env.REACT_APP_API_KEY
 
 export const YOUTUBE_VIDEOS_URL="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=100&regionCode=IN&key="+ API_KEY
 
 export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
-export const YOUTUBE_SEARCH_RESULTS_API=`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=AIzaSyBnz2RSRoyRpN8ofvYtfutbIn-S-lBAINI`
+export const YOUTUBE_SEARCH_RESULTS_API=`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=`+API_KEY
 
 export const YOUTUBE_VIDEO_BY_ID=`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=%s&key=%s`
 
